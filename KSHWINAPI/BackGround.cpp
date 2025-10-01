d01315aa02534fe72fbae1fded229e6ffcb2b6b0
KSH <tomato6082@naver.com>
2025-10-01 11:52:29 +0900

    1001-실습전 커밋

--- KSHWINAPI/BackGround.cpp
+++ KSHWINAPI/BackGround.cpp
@@ -1,65 +1,49 @@
 #include "Background.h"
 
 Background::Background(const wchar_t* InImagePath)
+    :Actor(InImagePath) 
 {
     Pivot.X = 0;    // 왼쪽 위가 피봇
     Pivot.Y = 0;
-    Position.Y = -PixelSize;
-    Image = new Gdiplus::Bitmap(InImagePath); // 이미지 로딩
-    if (Image->GetLastStatus() != Gdiplus::Ok)
-    {
-        // 정상적으로 파일 로딩이 안됬다.
-        delete Image;       // 실패했으면 즉시 해제
-        Image = nullptr;
-        OutputDebugString(L"플레이어 이미지 로드 실패");
-        MessageBox(g_hMainWindow, L"플레이어 이미지 로드 실패", L"오류", MB_OK | MB_ICONERROR);
-    }
+   
 }
 
-Background::~Background()
+void Background::OnTick(float inDealtaTIme)
 {
-    if (Image)
-    {
-        delete Image;
-        Image = nullptr;
-    }
+    Offset += ScrollSpeed * inDealtaTIme;
 }
 
-void Background::Tick(float inDealtaTIme)
+void Background::OnRender(Gdiplus::Graphics* InGraphics)
 {
-    Offset += Speed * inDealtaTIme;
-}
-
-void Background::Render(Gdiplus::Graphics* InGraphics)
-{
+    constexpr int PixelGap = 3;
     if (Image)
     {
         // Image가 로딩되어 있다.
-        int NewY = static_cast<int>(Position.Y - PixelSize * Pivot.Y + Offset);
+        int NewY = static_cast<int>(Position.Y - Size * Pivot.Y + Offset);
         if (Offset > g_ScreenSize.Y)
         {
-            Offset = -PixelSize;
+            Offset = -static_cast<float>(Size);
         }
 
-        int WidthCount = g_ScreenSize.X / PixelSize + 1;
-        int HeightCount = g_ScreenSize.Y / PixelSize + 2;
-        int TotalHeight = (PixelSize - 3) * HeightCount;
+        int WidthCount = g_ScreenSize.X / Size + 1;
+        int HeightCount = g_ScreenSize.Y / Size + 2;
+        int TotalHeight = (Size - PixelGap) * HeightCount;
 
         for (int y = -1; y < HeightCount; y++)
         {
             for (int x = 0; x < WidthCount; x++)
             {
-                int NewX = static_cast<int>(Position.X - PixelSize * Pivot.X + (PixelSize - 3) * x);
+                int NewX = static_cast<int>(Position.X - Size * Pivot.X + (Size - PixelGap) * x);
                 InGraphics->DrawImage(
                     Image,          // 그려질 이미지
                     NewX,           // 그려질 위치
                     NewY,
-                    PixelSize, PixelSize);  // 그려질 사이즈
+                    Size, Size);  // 그려질 사이즈
             }
-            NewY += (PixelSize - 3);
+            NewY += (Size - PixelGap);
             if (NewY > TotalHeight)
             {
-                NewY -= (TotalHeight + (PixelSize - 3));
+                NewY -= (TotalHeight + (Size - PixelGap));
             }
         }
     }
